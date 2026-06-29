e1b5c70fe86cab18793e740442981f05a3cb56c2
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-06-29 14:19:20 +0300

    feat: add environment variable configuration and update API URL handling

--- frontend/src/static/api/apis.ts
+++ frontend/src/static/api/apis.ts
@@ -1,9 +1,11 @@
 import { postJson } from "../services/services";
+import { API_URL } from "../constants/constants";
 
 export async function predict(comment: string) {
     const payload = {
         input: comment
     }
-    const prediction = postJson("http://127.0.0.1:8000/predict", payload)
+    const endpoint = "/predict"
+    const prediction = postJson(`"${API_URL}${endpoint}"`, payload)
     return prediction
 }
