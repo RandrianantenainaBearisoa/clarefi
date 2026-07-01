71afed0f699e58e2e4ae4ca5c8fd064836d93775
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-07-01 14:24:36 +0300

    fix: use argparse instead of sys

--- toto.py
+++ toto.py
@@ -1,4 +1,8 @@
-import sys
+import argparse
 
-commit_type = sys.argv[1]
+parser = argparse.ArgumentParser(description="A script that modify the image version in dockerhub.")
+parser.add_argument("--commit", type=str, default="fix", help="the type of action in the commit message")
+args = parser.parse_args()
+
+commit_type = args.commit
 print(commit_type)
