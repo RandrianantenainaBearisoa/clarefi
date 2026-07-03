7452f577f2d38970a09175b2fbf0163679b454a2
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-07-03 12:26:41 +0300

    fix: update frontend port mapping in docker-compose.yml and correct string interpolation in apis.ts

--- frontend/src/static/api/apis.ts
+++ frontend/src/static/api/apis.ts
@@ -6,6 +6,6 @@ export async function predict(comment: string) {
         input: comment
     }
     const endpoint = "/predict"
-    const prediction = postJson(`"${API_URL}${endpoint}"`, payload)
+    const prediction = postJson(`${API_URL}${endpoint}`, payload)
     return prediction
 }
