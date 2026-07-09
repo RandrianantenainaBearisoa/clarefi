4e1f6941f6cb117b9d1ab78615f95aa114d55543
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-07-09 10:52:22 +0300

    feat: add run_full_pipeline script to begin the project

---
+++ backend/run_full_pipeline.sh
@@ -0,0 +1,16 @@
+uv sync --frozen
+
+source .venv/bin/activate
+
+if [[ -n "$(which python)" ]]; then
+    echo "Virtual env. activated"
+    echo "Launching Data pipeline ..."
+    python launch_data_pipeline.py # download the dataset from source
+    echo -e "Dataset ready for training \nStart Training ..."
+    python train_prod_model.py # train locally the model in production (model_on_prod from model_config.yaml)
+    echo -e "The model on prod is now in the artefact store \nYou can begin to do experiments with the grid search_config.yaml && launch_grid_search.py \nThe result of the experiment can be desplayed by the script show_best_model.py (ordered by accuracy) like below"
+    python show_best_model.py
+else
+    echo "Virtual env. not activated"
+fi
+
