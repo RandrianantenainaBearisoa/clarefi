235682a25b4156fb9e6ea82d525aa2de7958fa5f
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-07-14 11:20:13 +0300

    refactor: remove commented-out out_of_vocab_rate implementation and unused imports

--- backend/src/core/inference/inference.py
+++ backend/src/core/inference/inference.py
@@ -1,5 +1,5 @@
 import joblib
-from src.core.utils.helpers import get_current_model, get_words_number, get_word_list
+from src.core.utils.helpers import get_current_model
 
 class Inference:
 
@@ -20,15 +20,6 @@ class Inference:
         oov_rate = self.out_of_vocab_rate(input=input)
 
         return prediction.tolist(), prediction_probability, oov_rate
-    
-    # def out_of_vocab_rate(self, input:str|list):
-    #     if not input:
-    #         return 0.0
-    #     words_list = get_word_list(sentence=input)
-    #     oov_nb = sum(1 for word in words_list if word not in self.vocab_set)
-    #     words_number = get_words_number(input)
-
-    #     return (oov_nb / words_number) * 100
     
     def out_of_vocab_rate(self, input:str|list):
         if isinstance(input, str):
